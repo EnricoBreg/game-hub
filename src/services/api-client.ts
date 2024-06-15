0755d24e9e0aa0e8a144d26730c1012df5ad2996
Enrico <bregolienrico@gmail.com>
2024-06-15 14:11:44 +0200

    Riscritto un commento per chiarezza

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -8,7 +8,7 @@ export interface FetchResponse<T> {
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    // rawg api key, non è buona pratica
+    // rawg api key, esporre in chiaro la chiave non è buona pratica
     // metterla in chiaro così, solo a scopi educativi
     key: "932d9af763714c16831f7933e6a05313",
   },
